b8d5346b23167cab7cd21c0b8508341b2e1e9c2b
toriringo <50064413+toriringo@users.noreply.github.com>
2019-08-10 15:42:46 +0900

    Update vendingmachine.h

--- vendingmachine.h
+++ vendingmachine.h
@@ -7,20 +7,22 @@ class VendingMachine
 public:
 	VendingMachine();
 	void run();
+
 private:
-    typedef struct
-    {
-        // 商品番号
-        int number;
-        // キー, 商品名, 価格
-	    std::unordered_map<std::string, std::pair<std::string, int>> product;
-    }Product;
-    Product products;
+	typedef struct
+	{
+		// 商品番号
+		int number;
+		// キー, 商品名, 価格
+		std::unordered_map<std::string, std::pair<std::string, int>> product;
+	}Product;
+
+	Product products;
 
 	void initializeProduct();
 	void showProduct();
 	int waitCharge();
    	int waitChoose();
-    Product *check(const int charge, const int choose);
-    void giveProduct(Product *p);
+	Product *check(const int charge, const int choose);
+	void giveProduct(Product *p);
 };
